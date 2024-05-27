d8100c40cf0eb9e0ada3fdb6758e23a66369aa1c
eerieA <ninja.duan@gmail.com>
2024-05-27 14:54:06 -0700

    Abstracted window into a class

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,17 @@
 #include <GLFW/glfw3.h>
 #include <glfw_initialization.h>
 #include <glfw_monitor.h>
+#include <glfw_window.h>
 
 std::int32_t main(std::int32_t argc, gsl::zstring* argv) {
 
     const veng::GlfwInitialization _glfw;
 
-    gsl::not_null<GLFWwindow *> window = glfwCreateWindow(800, 600, "Vulkan Engine", nullptr, nullptr);
-    gsl::final_action _cleanup_window([window]() { glfwDestroyWindow(window); });
-
-    glm::ivec2 window_size;
-    glfwGetWindowSize(window, &window_size.x, &window_size.y);
+    veng::Window window("Vulkan Engine", {800, 600});
+    window.TryMoveToMonitor(0);     // default to 0, change to other if needed
     
-    gsl::span<GLFWmonitor*> monitors = veng::GetMonitors();
-    if (monitors.size() > 0) {
-        veng::MoveWindowToMonitor(window, monitors[0]);
-    }
-
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
+    while (!window.ShouldClose()) {
+        glfwPollEvents();   // not window specific
     }
 
     return EXIT_SUCCESS;
